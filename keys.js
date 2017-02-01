4d8dfe8156f1f06918844e3b23adf330ea089fc4
bklynate <nathanielcfa@gmail.com>
2017-01-31 23:05:12 -0500

    initial commit

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('Locked and Loaded!');
+
+exports.twitterKeys = {
+  consumer_key: 'L1KL69qkKihj8d4s1xF6AOPJE',
+  consumer_secret: 'UiwSAYgY9FuIdscWL3AqMEsvhltkSo35PFz784rI6oJAC2Zg8H',
+  access_token_key: '818260356569112576-pfwZ771A1aIrQdAtKB5JHdiWGSS8lza',
+  access_token_secret: 'L25DId1lsaYqV41TzrfVZyrQ2t4md7gGgUNQEjF2Km7QC',
+}
---
+++ tara.js
@@ -0,0 +1,3 @@
+var twitterKeys = require('./keys')
+
+console.log(twitterKeys);
